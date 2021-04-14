160333ec75d975f0a0d93118bae8124cb7e046f0
Woohyun Shin <cpu344@gmail.com>
2021-04-14 21:53:32 +0900

    Make SetInputHandler public

--- src/Input/Input.cs
+++ src/Input/Input.cs
@@ -2,7 +2,7 @@ namespace Flora.Input {
     public static class Input {
         internal static InputHandler handler = null;
 
-        static void SetInputHandler(InputHandler handler) {
+        public static void SetInputHandler(InputHandler handler) {
             Input.handler = handler;
         }
     }
