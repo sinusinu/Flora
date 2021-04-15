2f4bfd148e94a0c7b0881272deda33e5571ca59a
Woohyun Shin <cpu344@gmail.com>
2021-04-15 14:47:05 +0900

    Make isAudioInitialized internal

--- src/Audio/Audio.cs
+++ src/Audio/Audio.cs
@@ -3,7 +3,7 @@ using SDL2;
 
 namespace Flora.Audio {
     public static class Audio {
-        static bool isAudioInitialized = false;
+        internal static bool isAudioInitialized = false;
 
         internal static void Init() {
             isAudioInitialized = true;
