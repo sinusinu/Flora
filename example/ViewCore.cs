54a1ac76d738158fd845eb9a03b7e8d38e86b2f9
Woohyun Shin <cpu344@gmail.com>
2021-04-18 21:15:08 +0900

    Add transform and zoom test in ViewCore

--- example/ViewCore.cs
+++ example/ViewCore.cs
@@ -8,6 +8,11 @@ namespace FloraExample {
         Texture texture;
         View view;
 
+        float brrr;
+        float viewPosX;
+        float viewPosY;
+        float viewZoom;
+
         public override void Prepare() {
             texture = new Texture(Path.Relative("res/480.png"));
             
@@ -22,9 +27,26 @@ namespace FloraExample {
         public override void Resize(int width, int height) {}
 
         public override void Render(float delta) {
+            brrr += delta * 0.005f;
+            viewPosX = MathF.Sin(brrr) * 40f;
+            viewPosY = MathF.Cos(brrr) * 40f;
+            viewZoom = (MathF.Sin(brrr) * 0.2f) + 0.8f;
+
+            view.SetPosition(viewPosX, viewPosY);
+            view.SetZoom(viewZoom);
+
             Gfx.Begin(new Color(0x00, 0x00, 0x00, 0xFF));
-            Gfx.Draw(texture, -428, 0);
-            Gfx.Draw(texture, 428, 0);
+            
+            Gfx.Draw(texture, -427, 0);
+            Gfx.Draw(texture, 426, 0);
+            Gfx.Draw(texture, 1279, 0);
+            
+            Gfx.SetColor(0, 255, 255, 255);
+            Gfx.DrawRect(new Rect(-5 + 427, -5 + 240, 10, 10), true);
+            Gfx.DrawRect(new Rect(10 + 427, -40 + 240, 20, 80), true);
+            Gfx.DrawLine(-20 + 427, -20 + 240, 20 + 427, -20 + 240);
+            Gfx.SetColor(255, 255, 255, 255);
+            
             Gfx.End();
         }
         
