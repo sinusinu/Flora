df5112f2b3f653668b7992f9d589b9d58793d82f
Woohyun Shin <cpu344@gmail.com>
2021-04-21 15:18:15 +0900

    Add Flora.Util.Math class
    It will provide simple math-related boilerplate functions like RadToDeg
    or DegToRad.

---
+++ src/Util/Math.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Flora.Util { 
+    class Math {
+        /// <summary>
+        /// Convert Radian angle to Degree angle.
+        /// </summary>
+        public static float RadToDeg(float rad) { return rad * 180f / MathF.PI; }
+        
+        /// <summary>
+        /// Convert Degree angle to Radian angle.
+        /// </summary>
+        public static float DegToRad(float deg) { return deg * MathF.PI / 180f; }
+    }
+}
