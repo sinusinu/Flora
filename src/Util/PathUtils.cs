3043bd7925d896177c4a0c226a75f6d1ace31db7
Woohyun Shin <cpu344@gmail.com>
2021-05-05 13:33:18 +0900

    PathUtils: Add doc to Relative

--- src/Util/PathUtils.cs
+++ src/Util/PathUtils.cs
@@ -6,7 +6,12 @@ namespace Flora.Util {
     /// </summary>
     public static class PathUtils {
         private static string executablePath = null;
-        
+
+        /// <summary>
+        /// Convert executable-relative path to accessible absolute path. This function does not get affected by working directory.
+        /// </summary>
+        /// <param name="path">executable-relative path</param>
+        /// <returns>absolute path</returns>
         public static string Relative(string path) {
             if (executablePath == null) {
                 executablePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
