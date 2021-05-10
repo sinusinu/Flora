367a6aefb21e84e8c2e4233f9feec48ffa8c5cec
Woohyun Shin <cpu344@gmail.com>
2021-05-10 14:24:17 +0900

    PathUtils: Use AppContext.BaseDirectory on Relative
    According to docs, Assembly.Location which current method
    relies on will return empty string on single-file deployment.
    It suggests using AppContext.BaseDirectory instead.

--- src/Util/PathUtils.cs
+++ src/Util/PathUtils.cs
@@ -5,19 +5,13 @@ namespace Flora.Util {
     /// Helper class for generating file paths.
     /// </summary>
     public static class PathUtils {
-        private static string executablePath = null;
-
         /// <summary>
         /// Convert executable-relative path to accessible absolute path. This function does not get affected by working directory.
         /// </summary>
         /// <param name="path">executable-relative path</param>
         /// <returns>absolute path</returns>
         public static string Relative(string path) {
-            if (executablePath == null) {
-                executablePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
-            }
-
-            return System.IO.Path.Join(executablePath, path);
+            return System.IO.Path.Join(AppContext.BaseDirectory, path);
         }
     }
 }
