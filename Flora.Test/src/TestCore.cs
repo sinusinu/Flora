63ab4102e995a7cc77c5e1d2376b20b77328123e
Woohyun Shin <cpu344@gmail.com>
2026-09-24 15:30:44 +0900

    Testing

--- Flora.Test/src/TestCore.cs
+++ Flora.Test/src/TestCore.cs
@@ -58,24 +58,21 @@ public class TestCore : Core {
         Gfx.Camera.ScaleX = intermediateSX;
         Gfx.Camera.ScaleY = intermediateSY;
 
-        // Gfx.Camera.PushState();
+        Gfx.Camera.PushState();
 
-        // Gfx.Camera.Rotation = MathF.Sin(dingus / 2f) * 40;
-        //Gfx.Camera.X = MathF.Sin(dingus) * 200;
-        // Gfx.Camera.ScaleX = ((MathF.Sin(dingus) + 1f) * 0.125f) + 1f;
-        // Gfx.Camera.ScaleY = ((MathF.Cos(dingus) + 1f) * 0.125f) + 1f;
+        Gfx.Camera.Rotation = MathF.Sin(dingus / 2f) * 40;
+        Gfx.Camera.X = MathF.Sin(dingus) * 200;
+        Gfx.Camera.ScaleX *= ((MathF.Sin(dingus) + 1f) * 0.125f) + 1f;
+        Gfx.Camera.ScaleY *= ((MathF.Cos(dingus) + 1f) * 0.125f) + 1f;
 
-        for (int y = -6; y < 6; y++) {
-            for (int x = -7; x < 7; x++) {
-                Gfx.RenderColor = new((x + 7) / 13f, (y + 6) / 11f, 1f, 1f);
+        for (int y = -11; y < 11; y++) {
+            for (int x = -12; x < 12; x++) {
+                Gfx.RenderColor = new((x + 12) / 24f, (y + 11) / 22f, 1f, 1f);
                 Gfx.Draw(texture, 128 * x, 128 * y, 128, 128, (spin ? dingus : 0) * (40 + ((x + y + 1) * 4)), 64, 64);
             }
         }
         
-        // font.Color = new Color(1f, 1f, 0f, 1f);
-        // font.Draw("이 텍스트는 카메라의\n영향을 받습니다.\nThe quick brown fox\njumps over the lazy dog", -200, -200);
-
-        // Gfx.Camera.PopState();
+        Gfx.Camera.PopState();
 
         Gfx.Draw(texture, 0, 0, 128, 128);
         font.Color = new Color(0f, 0f, 0f, 1f);
