2d2a0ffe715980933cba417600cc483f7c037da3
Woohyun Shin <cpu344@gmail.com>
2026-09-24 23:12:40 +0900

    Why is this public?

--- Flora/src/Input/Input.cs
+++ Flora/src/Input/Input.cs
@@ -6,7 +6,7 @@ public unsafe sealed class Input {
     private Application app;
 
     public enum ActiveInputOpts { None, Keyboard, Gamepad }
-    public ActiveInputOpts _activeInput = ActiveInputOpts.None;
+    private ActiveInputOpts _activeInput = ActiveInputOpts.None;
     public ActiveInputOpts ActiveInput {
         get => _activeInput;
         internal set {
